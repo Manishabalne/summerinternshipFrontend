b159319c8a692b2d69d41cc72c6278b8bd74f4b0
Manishabalne <164603005+Manishabalne@users.noreply.github.com>
2024-05-27 11:26:17 +0530

    Update Rootlayout.js

--- Components/Rootlayout.js
+++ Components/Rootlayout.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import {Outlet} from 'react-router-dom'
+import './Rootlayout.css'
 
 function Rootlayout() {
   return (
     <div>
         <Navbar/>
-        <div style={{minHeight:"100vh"}}><Outlet/></div>
+        <div style={{minHeight:"100vh"}} className='rootlayout'><Outlet/></div>
     </div>
   )
 }
